a93601419e1dc126b9b42d457d9f333a52e69299
seonggeunyu <167403364+seonggeunyu@users.noreply.github.com>
2024-09-12 15:11:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,110 +1,105 @@
 var getScriptPromisify = (src) => {
-	return new Promise((resolve) =>{
-	$.getScript(src, resolve)
-	})
+  return new Promise((resolve) => {
+    $.getScript(src, resolve)
+  })
 }
 
 var parseMetadata = metadata => {
-
-	const { dimensions : dimensionMap, mainStructureMembers : measureMap} = metadata
-	const dimensions = []
-	for (const key in dimensionMap)
-	{
-		const dimension = dimensionMap[key]
-		dimensions.push({key, ...dimension})
-	}
-
-	const measures = []
-
-	for (const key in measureMap)
-	{
-		const measure = measureMap[key]
-		measures.push({key, ...measure})
-	}
-
-	return {dimensions, measures, dimensionMap, measureMap}
-	
+  const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+  const dimensions = []
+  for (const key in dimensionsMap) {
+    const dimension = dimensionsMap[key]
+    dimensions.push({ key, ...dimension })
+  }
+  const measures = []
+  for (const key in measuresMap) {
+    const measure = measuresMap[key]
+    measures.push({ key, ...measure })
+  }
+  return { dimensions, measures, dimensionsMap, measuresMap }
 }
 
 (function () {
-const template = document.createElement('template')
-template.innerHTML = `
-<style>
-</style>
-<div id="root" style="width: 100%; height: 100%;">
-Hello Web Component
-</div>
-`
-class Main extends HTMLElement {
-constructor () {
-super()
-this._shadowRoot = this.attachShadow({ mode: 'open' })
-this._shadowRoot.appendChild(template.content.cloneNode(true))
-this._root = this._shadowRoot.getElementById('root')
-this._eChart = null
-}
-onCustomWidgetResize (width, height){
-	this.render();
-}
+  const template = document.createElement('template')
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `
+  class Main extends HTMLElement {
+    constructor () {
+      super()
 
-onCustomWidgetAfterUpdate(changedProps){
-	this.render();
-}
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-onCustomWidgetDestroy(){
-		//
-}
-	
-async render(){
-	
-	const dataBinding = this.dataBinding
-	if(!dataBinding || dataBinding.state !== 'success')
-	{
-		return
-	}	
+      this._root = this._shadowRoot.getElementById('root')
 
-	await getScriptPromisify('https://cdn.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js')
+      this._eChart = null
+    }
 
-	const {data, metadata} = dataBinding
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
 
-	const {dimensions, measures} = parseMetadata(metadata)
+    onCustomWidgetAfterUpdate (changedProps) {
+      this.render()
+    }
 
-	const categoryData = []
+    onCustomWidgetDestroy () {
+      if (this._eChart && echarts) { echarts.dispose(this._eChart) }
+    }
 
-	const series = measures.map(measure => {
-		return {
-			id: measure.id,
-			name: measure.label,
-			data: [],
-			key: measure.key,
-			type:'line',
-			smooth:true
-		}
-	})
+    setSeriesType (seriesType) {
+      this.seriesType = seriesType
+      this.dispatchEvent(new CustomEvent('propertiesChanged', { detail: { properties: { seriesType } } }))
+      this.render()
+    }
 
-	data.forEach(row => {
-		categoryData.push(dimensions.map(dimension => {
-			return row[dimension.key].label
-		}).join('/'))
-		series.forEach(series => {
-			series.data.push(row[series.key].raw)
-		})
-	})
+    async render () {
+      const dataBinding = this.dataBinding
+      if (!dataBinding || dataBinding.state !== 'success') { return }
 
-	if(this._eChart) { echarts.dispose(this._eChart)}
-	const eChart = this._eChart = echarts.init(this._root, 'main')
-	const option = {
-		xAxis: { type : 'category' , data : categoryData},
-		yAxis: { type : 'value'},
-		tooltips: {trigger:'axis'},
-		series
-	}
-	eChart.setOption(option)
-			
-	//this._root.textContent = JSON.stringify(dataBinding)
-	
-	//this._root.textContent = `Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight}`
-}
-}
-customElements.define('com-sap-sac-exercise-ysg001-main', Main)
+      await getScriptPromisify('https://cdn.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js')
+
+      const { data, metadata } = dataBinding
+      const { dimensions, measures } = parseMetadata(metadata)
+      // dimension
+      const categoryData = []
+
+      // measures
+      const series = measures.map(measure => {
+        return {
+          id: measure.id,
+          name: measure.label,
+          data: [],
+          key: measure.key,
+          type: this.seriesType || 'line',
+          smooth: true
+        }
+      })
+
+      data.forEach(row => {
+        categoryData.push(dimensions.map(dimension => {
+          return row[dimension.key].label
+        }).join('/')) // dimension
+        series.forEach(series => {
+          series.data.push(row[series.key].raw)
+        }) // measures
+      })
+
+      if (this._eChart) { echarts.dispose(this._eChart) }
+      const eChart = this._eChart = echarts.init(this._root, 'main')
+      const option = {
+        xAxis: { type: 'category', data: categoryData },
+        yAxis: { type: 'value' },
+        tooltip: { trigger: 'axis' },
+        series
+      }
+      eChart.setOption(option)
+    }
+  }
+
+  customElements.define('com-sap-sac-exercise-ysg001-main', Main)
 })()
